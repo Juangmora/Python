2ab11a9b185afc7c54236cfd8413801612e77fef
Juangmora <jmorales341@students.mchenry.edu>
2025-09-02 18:00:35 -0500

    program that shows variables examples

---
+++ sample program.py
@@ -0,0 +1,22 @@
+"""Copy the sample program into your Python editor. Run the program and observe the output. Change the variable values, test the program, and submit your file."""
+# This is a Python program to demonstrate different data types
+
+# Literal
+print("Literal:", 45)
+
+# Integer
+y = -35
+print("Integer:", y)
+
+# Float
+z = 5.5
+print("Float:", z)
+
+# String
+name = "Juan M"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
+      
