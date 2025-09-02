6905c6810f974674a769ce89b95e8a376d0e6264
Juangmora <jmorales341@students.mchenry.edu>
2025-09-02 16:43:02 -0500

    Add files via upload

---
+++ knock knock.py
@@ -0,0 +1,2 @@
+#Knock knock joke as one line of code using escape characters
+print("Knock, knock.\nWho's there?\nWater.\nWater who?\nWater you doing telling jokes right now? Don't you have things to do?")
