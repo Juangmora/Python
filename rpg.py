fa28c2560a8f6abf52e4cced0a61064b0e0a8263
Juangmora <jmorales341@students.mchenry.edu>
2026-01-27 01:07:58 -0600

    This is my rpg character.

---
+++ rpg.py
@@ -0,0 +1,22 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS (DO NOT DELETE)
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included.
+[ ] 2. Define a String variable.
+[ ] 3. Define an Integer variable.
+[ ] 4. Define a Float variable.
+[ ] 5. Define a Boolean variable.
+[ ] 6. Print all variables using F-Strings.
+[ ] 7. Upload to GitHub.
+-----------------------------------------------------------------------
+"""
+
+# My rpg character stats
+hero_name = "Darkside" #string
+specialty = "Dragoon" # string
+level = 25 # integer
+agility = 65.90 # float
+full_health = True # booleon
+
+print (f"I would like to introduce {hero_name}!\nHe is a level {level} {specialty} with {agility} rating in speed.\nHis health is currently at 100%. {full_health}")
